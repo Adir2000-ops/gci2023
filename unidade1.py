a80bd141adfb8cdff074a1357b3bfde6515e6c30
Adir Venturi <adir.junior@unidavi.edu.br>
2023-02-28 00:08:07 +0000

    commit 2

---
+++ unidade1.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+@st.cache_data
+def load_database():
+    return pd.read_csv('brasil_estados.csv')
+
+st.title('Meu primeiro App - GCI')
+
+estados = load_database()
+st.dataframe(estados)
+
+dados, estatistica = st.tabs(['Dados', 'Estatística Descritiva'])
+
+with dados:
+    regiao = st.selectbox(
+        'Selecione a regiao:',
+        estados['regiao_nome'].unique()
+    )
+    st.dataframe(estados[estados['regiao_nome'] == regiao])
